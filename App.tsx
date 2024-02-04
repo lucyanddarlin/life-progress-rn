b9168a822e2390cd06549cc44a787e630149798f
lucy.li <lucy.li@quectel.com>
2024-02-04 15:03:21 +0800

    feat: config react-native-reanimated

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, View} from 'react-native';
+import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
+
 function App(): React.JSX.Element {
+  const width = useSharedValue(100);
+
+  const onPress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
   return (
-    <View>
-      <Text>demo</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: '#b58df1',
+          marginBottom: 30,
+          borderRadius: 20,
+        }}
+      />
+      <Button onPress={onPress} title="Click me" />
+      <Button
+        onPress={() => {
+          width.value = withSpring(100);
+        }}
+        title="Reset"
+      />
     </View>
   );
 }
